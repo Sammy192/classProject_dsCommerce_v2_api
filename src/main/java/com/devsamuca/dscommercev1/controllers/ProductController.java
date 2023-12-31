a2c1a04206093e7ebfcfea1ca48e0ead57f31b03
Samuel R Borges <sammy_rb@hotmail.com>
2023-12-31 16:38:26 -0300

    created delete product

--- src/main/java/com/devsamuca/dscommercev1/controllers/ProductController.java
+++ src/main/java/com/devsamuca/dscommercev1/controllers/ProductController.java
@@ -46,4 +46,24 @@ public class ProductController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto  = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+
+    /* Outro delete, usando o ResponseEntity para tratar retorno do cod status
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    } */
+
 }
